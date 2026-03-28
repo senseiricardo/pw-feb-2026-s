039031fd0ec1a33bc8a8a5930524aeb5939cc309
senseiricardo <ricardo.avalos@senseitech.io>
2026-03-28 09:51:54 -0600

    users

---
+++ data/users.js
@@ -0,0 +1,15 @@
+export const users = {
+    standard: {
+        username: 'standard_user',
+        password: 'secret_sauce',
+    },
+    invalid: {
+        username: 'standard_user',
+        password: 'wrong_password',
+    },
+    locked: {
+        username: 'locked_out_user',
+        password: 'wrong_password',
+    }
+
+}
